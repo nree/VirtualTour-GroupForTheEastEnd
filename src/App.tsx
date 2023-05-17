d78939602bd74844aa3138c47420786614dba68d
nree <yhenrc@gmail.com>
2023-05-16 21:04:01 -0400

    fix routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 import Home from './components/Home'
 import Preview from './components/Preview'
-import { BrowserRouter, Route, Routes } from 'react-router-dom'
+import { BrowserRouter, Route, Routes} from 'react-router-dom'
 import { Provider } from 'react-redux'
 import { store } from './store'
 import VirtualTour from './components/VirtualTour'
@@ -11,7 +11,8 @@ function App() {
       <BrowserRouter>
         <Routes>
           <Route path='/virtualtour' element={<VirtualTour />} />
-          <Route path='/*' index element={<Home />} />
+
+          <Route path='/*' element={<Home />} />
         </Routes>
       </BrowserRouter>
     </Provider>
